8336ed21f888b533dc28041c583b89c00c8391c3
An Luong <anmluong@gmail.com>
2020-05-18 23:44:48 -0400

    layout updates + extras
    changed layout of case studies, added more information to ARChef, removed Circ, added Google Analytics

--- main.js
+++ main.js
@@ -12,4 +12,33 @@ $(document).ready(function() {
     }, 125);
     return false;
   });
+
+  $('#scroll').on("keydown", function(event){
+    if (event.keyCode == 13) {
+      $("html, body").animate({
+      scrollTop: 0
+    }, 125);
+    return false;
+    }
+  });
+});
+
+$('#carousel').on('keydown', function(e) {
+    if (e.keyCode == 37) {  // left arrow
+        $('#carousel').carousel('prev')
+    } else {
+        if (e.keyCode == 39) {  // right arrow
+            $('#carousel').carousel('next')
+        }
+    }
+});
+
+$('#carousel2').on('keydown', function(e) {
+    if (e.keyCode == 37) {  // left arrow
+        $('#carousel2').carousel('prev')
+    } else {
+        if (e.keyCode == 39) {  // right arrow
+            $('#carousel2').carousel('next')
+        }
+    }
 });
